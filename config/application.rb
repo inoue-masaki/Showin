fa5cc38005930dba0a4ee6109773dce842b67924
Ubuntu <ubuntu@ip-10-0-1-175.ap-northeast-1.compute.internal>
2020-06-10 06:24:40 +0000

    aa

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -16,6 +18,7 @@ module HelloApp
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.initialize_on_precompile = false 
   end
 end
 
