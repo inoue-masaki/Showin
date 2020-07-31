64040c917748c6aadcff65c34f65b00f25d896a5
Ubuntu <ubuntu@ip-10-0-1-41.ap-northeast-1.compute.internal>
2020-07-31 13:38:40 +0000

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '〇〇.herokuapp.com'
+  host = 'https://obscure-dusk-16737.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
