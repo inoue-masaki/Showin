4cb2781e2930926cc0cd32f03e79d9a7e52149c1
Ubuntu <ubuntu@ip-10-0-1-41.ap-northeast-1.compute.internal>
2020-07-31 11:33:35 +0000

    Write Ruby Version in database2

--- config/puma.rb
+++ config/puma.rb
@@ -56,6 +56,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
